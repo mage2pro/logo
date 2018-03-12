1ee6073a257ecda8d6294c7c32c8145266b92c6e
Dmitry Fedyuk <dfediuk@gmail.com>
2018-03-13 04:11:32 +0600

    no message

--- view/frontend/web/main.js
+++ view/frontend/web/main.js
@@ -8,9 +8,23 @@ define(['df', 'df-lodash', 'jquery'], function(df, _, $) {return (
 	function(config, element) {
 		/** @type {jQuery} HTMLDivElement */ var $c = $(element);
 		/** @type {jQuery} HTMLDivElement */ var $main = $('.product.media');
+		var left = 248;
+		var top = 140;
+		var scale = 0.4;
 		var $logo = $('<img>').attr('class', 'dfe-logo-applied').hide().prependTo($main);
 		$('img', $c).click(function() {
-			$logo.attr('src', this.src).show();
+			var $this = $(this);
+			var h = $this.height() * scale;
+			var w = $this.width() * scale;
+			$logo
+				.attr('src', this.src)
+				.css({
+					'margin-left': (left - w / 2) + 'px'
+					,'margin-top' : (top - h / 2) + 'px'
+					,height: h + 'px'
+				})
+				.show()
+			;
 		});
 	});
 });
