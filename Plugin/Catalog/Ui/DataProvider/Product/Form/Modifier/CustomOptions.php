85239ac0012589383d0a6766f8ebee8a753eabe8
Dmitry Fedyuk <dfediuk@gmail.com>
2018-03-13 02:17:25 +0600

    0.0.2

---
+++ Plugin/Catalog/Ui/DataProvider/Product/Form/Modifier/CustomOptions.php
@@ -0,0 +1,3 @@
+<?php
+namespace Dfe\Logo\Plugin\Catalog\Ui\DataProvider\Product\Form\Modifier;
+final class CustomOptions {}
---
+++ Validator.php
@@ -0,0 +1,8 @@
+<?php
+namespace Dfe\Logo;
+use Magento\Framework\Validator\AbstractValidator;
+// 2018-03-12
+/** @final Unable to use the PHP «final» keyword here because of the M2 code generation. */
+class Validator extends AbstractValidator {
+	function isValid($v) {return true;}
+}
