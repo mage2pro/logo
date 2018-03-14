105d4c08379facf8176d64ddc5f6e1412500d2d4
Dmitry Fedyuk <dfediuk@gmail.com>
2018-03-14 10:01:17 +0600

    1.0.4

--- Model/Observer/OptionSaveAfter.php
+++ Model/Observer/OptionSaveAfter.php
@@ -13,62 +13,58 @@ class OptionSaveAfter implements ObserverInterface
 
 
   function __construct(
-      \Magento\Catalog\Model\Product\Media\Config $mediaConfig,
-      \Magento\Framework\Filesystem $filesystem,
-      \Magento\Framework\ObjectManagerInterface $objectManager           
+	  \Magento\Catalog\Model\Product\Media\Config $mediaConfig,
+	  \Magento\Framework\Filesystem $filesystem,
+	  \Magento\Framework\ObjectManagerInterface $objectManager
   ) {        
-      $this->_mediaConfig     = $mediaConfig; 
-      $this->_mediaDirectory  = $filesystem->getDirectoryWrite(\Magento\Framework\App\Filesystem\DirectoryList::MEDIA);      
-      $this->_objectManager   = $objectManager;                          
+	  $this->_mediaConfig     = $mediaConfig;
+	  $this->_mediaDirectory  = $filesystem->getDirectoryWrite(\Magento\Framework\App\Filesystem\DirectoryList::MEDIA);
+	  $this->_objectManager   = $objectManager;
   } 
  
 
-  function execute(\Magento\Framework\Event\Observer $observer)
-  {
-  
+	function execute(\Magento\Framework\Event\Observer $observer) {
 		$object = $observer->getEvent()->getObject();
-
-    if ($object->getResourceName() == 'Magento\Catalog\Model\ResourceModel\Product\Option\Value'){
-				
+		if ($object->getResourceName() == 'Magento\Catalog\Model\ResourceModel\Product\Option\Value') {
 			$image = $object->getImage();
 			if (preg_match("/.tmp$/i", $image)) {
 				$image = $this->_moveImageFromTmp($image);
-			}	else if ($object->getImageSavedAs() && $object->getDeleteImage() != 1){
-			  $image = $object->getImageSavedAs();
-			}					
-				
-			if (!empty($image) || $object->getDeleteImage() == 1){
-			  $model = $this->_objectManager->create('Dfe\Logo\Model\Value')->load($object->getId(), 'option_type_id');
-        $model->setOptionTypeId($object->getId());        
-        $model->setImage($image);
-        $model->save();
-			}	
-
+			}
+			else if ($object['imageSavedAs'] && !$object->getDeleteImage()) {
+				$image = $object['imageSavedAs'];
+			}
+			// 2018-03-14
+			if ((!empty($image) || $object->getDeleteImage())) {
+				df_assert(!df_check_url_absolute($image));
+				$model = $this->_objectManager->create('Dfe\Logo\Model\Value')->load($object->getId(), 'option_type_id');
+				$model->setOptionTypeId($object->getId());
+				$model->setImage($image);
+				$model->save();
+			}
 		}
-
-    return $this;
-  }
+		return $this;
+	}
   
 	
 	
   protected function _moveImageFromTmp($file)
   {
-      if (strrpos($file, '.tmp') == strlen($file) - 4) {
-          $file = substr($file, 0, strlen($file) - 4);
-      }
+	  if (strrpos($file, '.tmp') == strlen($file) - 4) {
+		  $file = substr($file, 0, strlen($file) - 4);
+	  }
 
-      $destFile = dirname(
-          $file
-      ) . '/' . \Magento\MediaStorage\Model\File\Uploader::getNewFileName(
-          $this->_mediaDirectory->getAbsolutePath($this->_mediaConfig->getMediaPath($file))
-      );
-      
-      $this->_mediaDirectory->renameFile(
-        $this->_mediaConfig->getTmpMediaPath($file),
-        $this->_mediaConfig->getMediaPath($destFile)
-      );	
+	  $destFile = dirname(
+		  $file
+	  ) . '/' . \Magento\MediaStorage\Model\File\Uploader::getNewFileName(
+		  $this->_mediaDirectory->getAbsolutePath($this->_mediaConfig->getMediaPath($file))
+	  );
+
+	  $this->_mediaDirectory->renameFile(
+		$this->_mediaConfig->getTmpMediaPath($file),
+		$this->_mediaConfig->getMediaPath($destFile)
+	  );
 	
-      return str_replace('\\', '/', $destFile);
+	  return str_replace('\\', '/', $destFile);
   } 
   
   
