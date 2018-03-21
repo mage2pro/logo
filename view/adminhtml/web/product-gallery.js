d48e9bc6244d73828b97c31ab2c631e4f3e5b566
Dmitry Fedyuk <dfediuk@gmail.com>
2018-03-21 20:19:59 +0600

    no message

--- view/adminhtml/web/product-gallery.js
+++ view/adminhtml/web/product-gallery.js
@@ -1,12 +1,14 @@
 // 2018-03-21
 // http://devdocs.magento.com/guides/v2.0/javascript-dev-guide/javascript/js_mixins.html
 // https://magento.stackexchange.com/questions/142970
-define(['jquery'], function($) {'use strict';
+define(['jquery', 'df-lodash'], function($, _) {'use strict';
 return function(widget) {$.widget('mage.productGallery', widget, {
 	_addItem: function(event, imageData) {
-		this._super(event, $.extend(imageData, {
-			//logo_top: 50
-		}));
+		_.each(['logo_left', 'logo_top', 'logo_scale'], function(k) {
+			var v = imageData[k];
+			var vr = Math.round(v);	
+			imageData[k] = v == vr ? vr : v;
+		});
 	}
 	,_initDialog: function() {
 		this._super(); // 2018-03-21 http://api.jqueryui.com/jQuery.widget/#method-_super
