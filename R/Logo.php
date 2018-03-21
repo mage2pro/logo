32544d601433a81bf254e94a9ad926f35e71c7cb
Dmitry Fedyuk <dfediuk@gmail.com>
2018-03-21 11:35:36 +0600

    1.2.5

--- R/Logo.php
+++ R/Logo.php
@@ -21,13 +21,25 @@ class Logo extends \Df\Framework\Model\ResourceModel\Db\AbstractDb {
 	/**
 	 * 2018-03-21
 	 * @final Unable to use the PHP «final» keyword here because of the M2 code generation.
+	 * @param array(array(string => int|float)) $data
+	 * @param string[] $fields [optional]
+	 * @return int
+	 * @throws LE
+	 */
+	function insertOnDuplicate(array $data, array $fields = []) {return $this->c()->insertOnDuplicate(
+		$this->t(), $data, $fields
+	);}
+
+	/**
+	 * 2018-03-21
+	 * @final Unable to use the PHP «final» keyword here because of the M2 code generation.
 	 * @param int[] $ids
 	 * @return array(array(string => int|float))
 	 * @throws LE
 	 */
 	function loadByIds(array $ids) {
 		$c = $this->c(); /** @var Mysql|IAdapter $c */
-		$s = $c->select()->from($this->getMainTable())->where('value_id IN(?)', $ids); /** @var Select $s */
+		$s = $c->select()->from($this->t())->where('value_id IN(?)', $ids); /** @var Select $s */
 		return $c->fetchAll($s);
 	}
 }
