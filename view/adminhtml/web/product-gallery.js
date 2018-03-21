2fafa3ed242559d72a85df1ff0ba02ea04e0bac4
Dmitry Fedyuk <dfediuk@gmail.com>
2018-03-21 20:04:23 +0600

    no message

--- view/adminhtml/web/product-gallery.js
+++ view/adminhtml/web/product-gallery.js
@@ -4,9 +4,14 @@
 define(['jquery'], function($) {'use strict';
 return function(widget) {$.widget('mage.productGallery', widget, {
 	_addItem: function(event, imageData) {
-		// http://api.jqueryui.com/jQuery.widget/#method-_super
 		this._super(event, $.extend(imageData, {
-			logo_top: 50
+			//logo_top: 50
 		}));
+	}
+	,_initDialog: function() {
+		this._super(); // 2018-03-21 http://api.jqueryui.com/jQuery.widget/#method-_super
+		this.$dialog.on('change', '#logo_left,#logo_top,#logo_scale', $.proxy(function(e) {
+			this.$dialog.data('imageData')[e.currentTarget.id] = e.currentTarget.value;
+		}, this));
 	}
 });return $.mage.productGallery;};});
