743d979cf61224d913ac36c409111522e4a0329c
Dmitry Fedyuk <dfediuk@gmail.com>
2018-03-31 06:35:38 +0600

    1.3.4: https://github.com/mage2pro/logo/issues/1

--- Frontend.php
+++ Frontend.php
@@ -42,9 +42,20 @@ class Frontend extends _P {
 		 * @param int|float $d [optional]
 		 * @return array(string => int|float)
 		 */
-		$logoF = function(array $i, $k, $d = 0) use($p) {return [$k => floatval(dfa($i,
-			"logo_$k", dfa($i, "logo_{$k}_default", $p['scale' === $k ? "dfe_logo_$k" : "dfe_logo_offset_$k"] ?: $d)
-		))];};
+		$logoF = function(array $i, $k, $d = 0) use($p) {return [$k => floatval(
+			/**
+			 * 2018-03-31
+			 * 1) We should not use the 3-rd parameter of @uses dfa()
+			 * because it will not used in the 0 case.
+			 * «If I set global value its not working in any image»:
+			 * https://github.com/mage2pro/logo/issues/1
+			 * https://www.upwork.com/messages/rooms/room_b509c3b37b15c72c500ce79a2a9568c4/story_8d6ec5e96d68a60e4d313b3182a085a4
+			 * 2) @uses floatval() is required here too, because `'0.00' ?: 3` will return '0.00'.
+			 */
+			floatval(dfa($i, "logo_$k")) ?: (floatval(dfa($i, "logo_{$k}_default")) ?: (
+				floatval($p['scale' === $k ? "dfe_logo_$k" : "dfe_logo_offset_$k"]) ?: $d
+			))
+		)];};
 		$logos = df_eta(dfa(df_eta($p['media_gallery']), 'images'));
 		return !($images = $rc->getImagesWithOptionIdAndTitle($p->getId())) ? '' : df_cc_n(
 			df_tag('div',
@@ -53,7 +64,7 @@ class Frontend extends _P {
 					'logoId' => !$logos ? null : df_first($logos)['value_id']
 					,'logos' => df_map(function(array $i) use($logoF) {return
 						$logoF($i, 'left') + $logoF($i, 'top') + $logoF($i, 'scale', 0.4)
-						+ ['position' => intval(dfa($i, 'position', dfa($i, 'position_default', 0)))]
+						+ ['position' => intval(dfa($i, 'position')) ?: (intval(dfa($i, 'position_default')) ?: 0)]
 					;}, $logos)
 					,'optionId' => dfa(df_first($images), 'option_id')
 				])
