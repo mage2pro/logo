206615e6f2ad4121a3d3c521a811429d7ab06c1b
Dmitry Fedyuk <dfediuk@gmail.com>
2023-01-04 22:32:22 +0000

    refactoring

--- Controller/Adminhtml/Oi/Export.php
+++ Controller/Adminhtml/Oi/Export.php
@@ -1,25 +1,25 @@
 <?php
 namespace Dfe\Logo\Controller\Adminhtml\Oi;
 abstract class Export extends \Magento\Backend\App\AbstractAction {
-    protected $_productResource;
-    /**
-     * @var \Magento\Framework\App\Response\Http\FileFactory
-     */
-    protected $_fileFactory;
+	protected $_productResource;
+	/**
+	 * @var \Magento\Framework\App\Response\Http\FileFactory
+	 */
+	protected $_fileFactory;
 
-    function __construct(
-        \Magento\Backend\App\Action\Context $context,
-        \Magento\Catalog\Model\ResourceModel\Product $productResource,        
-        \Magento\Framework\App\Response\Http\FileFactory $fileFactory
-    ) {
-        $this->_productResource = $productResource;    
-        $this->_fileFactory = $fileFactory;
-        parent::__construct($context);
-    }
-    
-    
-    protected function _isAllowed()
-    {
-       return $this->_authorization->isAllowed('Dfe_Logo::export');
-    }     
+	function __construct(
+		\Magento\Backend\App\Action\Context $context,
+		\Magento\Catalog\Model\ResourceModel\Product $productResource,
+		\Magento\Framework\App\Response\Http\FileFactory $fileFactory
+	) {
+		$this->_productResource = $productResource;
+		$this->_fileFactory = $fileFactory;
+		parent::__construct($context);
+	}
+	
+
+	protected function _isAllowed()
+	{
+	   return $this->_authorization->isAllowed('Dfe_Logo::export');
+	}
 }
