215165f398dc2149800f0a617d6826a32a6a19b3
Dmitry Fedyuk <dfediuk@gmail.com>
2023-01-04 22:39:12 +0000

    refactoring

--- Controller/Adminhtml/Oi/Export/Import.php
+++ Controller/Adminhtml/Oi/Export/Import.php
@@ -1,23 +1,23 @@
 <?php
 namespace Dfe\Logo\Controller\Adminhtml\Oi\Export;
 class Import extends \Dfe\Logo\Controller\Adminhtml\Oi\Export {
-  function execute()
-  {
-	if ($this->getRequest()->isPost() && $this->getRequest()->getFiles('import_file')) {
-		try {
-
-			$importHandler = $this->_objectManager->create('Dfe\Logo\M\CsvImportHandler');
-			$importHandler->importFromCsvFile($this->getRequest()->getFiles('import_file'));
-
-			$this->messageManager->addSuccess(__('Product custom options have been imported.'));
-		} catch (\Magento\Framework\Exception\LocalizedException $e) {
-			$this->messageManager->addError($e->getMessage());
-		} catch (\Exception $e) {
-			$this->messageManager->addError($e->getMessage().__('Invalid file upload attempt'));
+	function execute() {
+		if ($this->getRequest()->isPost() && $this->getRequest()->getFiles('import_file')) {
+			try {
+				$importHandler = $this->_objectManager->create('Dfe\Logo\M\CsvImportHandler');
+				$importHandler->importFromCsvFile($this->getRequest()->getFiles('import_file'));
+				$this->messageManager->addSuccess(__('Product custom options have been imported.'));
+			}
+			catch (\Magento\Framework\Exception\LocalizedException $e) {
+				$this->messageManager->addError($e->getMessage());
+			}
+			catch (\Exception $e) {
+				$this->messageManager->addError($e->getMessage().__('Invalid file upload attempt'));
+			}
 		}
-	} else {
-		$this->messageManager->addError(__('Invalid file upload attempt'));
+		else {
+			$this->messageManager->addError(__('Invalid file upload attempt'));
+		}
+		$this->getResponse()->setRedirect($this->_redirect->getRedirectUrl($this->getUrl('*')));
 	}
-	$this->getResponse()->setRedirect($this->_redirect->getRedirectUrl($this->getUrl('*')));
-  }
 }
