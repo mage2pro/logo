ffe0d786b9d4837c31911c7be07494f108cf1942
Dmitry Fedyuk <dfediuk@gmail.com>
2023-01-04 22:48:15 +0000

    refactoring

--- M/Plugin/DuplicateProduct.php
+++ M/Plugin/DuplicateProduct.php
@@ -1,13 +1,13 @@
 <?php
 namespace Dfe\Logo\M\Plugin;
 class DuplicateProduct {
-    protected $_oiValue;
-    function __construct(\Dfe\Logo\M\Value $oiValue) {$this->_oiValue = $oiValue;}
-    function aroundDuplicate(
-    	\Magento\Catalog\Model\Product\Option\Value $value, \Closure $proceed, $oldOptionId, $newOptionId
+	protected $_oiValue;
+	function __construct(\Dfe\Logo\M\Value $oiValue) {$this->_oiValue = $oiValue;}
+	function aroundDuplicate(
+		\Magento\Catalog\Model\Product\Option\Value $value, \Closure $proceed, $oldOptionId, $newOptionId
 	) {
-        $result = $proceed($oldOptionId, $newOptionId);
-        $this->_oiValue->getResource()->duplicate($oldOptionId, $newOptionId);
-        return $result;
-    }
+		$result = $proceed($oldOptionId, $newOptionId);
+		$this->_oiValue->getResource()->duplicate($oldOptionId, $newOptionId);
+		return $result;
+	}
 }
