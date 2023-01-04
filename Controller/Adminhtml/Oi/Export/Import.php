55468d3c0d8d00b9a619b1e3635d3759f1d5a808
Dmitry Fedyuk <dfediuk@gmail.com>
2023-01-04 22:39:35 +0000

    refactoring

--- Controller/Adminhtml/Oi/Export/Import.php
+++ Controller/Adminhtml/Oi/Export/Import.php
@@ -2,7 +2,10 @@
 namespace Dfe\Logo\Controller\Adminhtml\Oi\Export;
 class Import extends \Dfe\Logo\Controller\Adminhtml\Oi\Export {
 	function execute() {
-		if ($this->getRequest()->isPost() && $this->getRequest()->getFiles('import_file')) {
+		if (!$this->getRequest()->isPost() && $this->getRequest()->getFiles('import_file')) {
+			$this->messageManager->addError(__('Invalid file upload attempt'));
+		}
+		else {
 			try {
 				$importHandler = $this->_objectManager->create('Dfe\Logo\M\CsvImportHandler');
 				$importHandler->importFromCsvFile($this->getRequest()->getFiles('import_file'));
@@ -14,9 +17,6 @@ class Import extends \Dfe\Logo\Controller\Adminhtml\Oi\Export {
 			catch (\Exception $e) {
 				$this->messageManager->addError($e->getMessage().__('Invalid file upload attempt'));
 			}
-		}
-		else {
-			$this->messageManager->addError(__('Invalid file upload attempt'));
 		}
 		$this->getResponse()->setRedirect($this->_redirect->getRedirectUrl($this->getUrl('*')));
 	}
