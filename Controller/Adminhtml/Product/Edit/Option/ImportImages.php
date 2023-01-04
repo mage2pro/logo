e1afb0adc811df2a7e0fcb8a5efa1370302194c6
Dmitry Fedyuk <dfediuk@gmail.com>
2023-01-04 22:43:45 +0000

    refactoring

--- Controller/Adminhtml/Product/Edit/Option/ImportImages.php
+++ Controller/Adminhtml/Product/Edit/Option/ImportImages.php
@@ -19,20 +19,21 @@ class ImportImages extends \Magento\Backend\App\AbstractAction {
 		parent::__construct($context);
 	}
 
-  function execute()
-  { 
-   
-	$config = array('image' => [], 'imageSavedAs' => []);
-
-	$productId = (int) $this->getRequest()->getParam('product_id');
-
-	$images = $this->_oiValue->getImages($productId);
-	foreach ($images as $id => $image) {
-	  $valueId = (int) $id;
-	  $config['image'][$valueId] = $this->_imageHelper->init($this->_product, 'product_page_image_small', array('type'=>'thumbnail'))->resize(40)->setImageFile($image)->getUrl();
-	  $config['imageSavedAs'][$valueId] = $image;
+	function execute() {
+		$config = array('image' => [], 'imageSavedAs' => []);
+		$productId = (int) $this->getRequest()->getParam('product_id');
+		$images = $this->_oiValue->getImages($productId);
+		foreach ($images as $id => $image) {
+			$valueId = (int) $id;
+			$config['image'][$valueId] =
+				$this->_imageHelper
+					->init($this->_product, 'product_page_image_small', array('type'=>'thumbnail'))
+					->resize(40)
+					->setImageFile($image)
+					->getUrl()
+			;
+			$config['imageSavedAs'][$valueId] = $image;
+		}
+		$this->getResponse()->setBody($this->_jsonEncoder->encode($config));
 	}
-
-	$this->getResponse()->setBody($this->_jsonEncoder->encode($config));
-  }
 }
